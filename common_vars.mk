0a8acd9214e68c65be9ab2cfc0f0b7de59d7541e
MisaghM <87766065+MisaghM@users.noreply.github.com>
2022-02-25 00:39:06 +0330

    Only use Bash commands

--- common_vars.mk
+++ common_vars.mk
@@ -6,20 +6,10 @@ CPPFLAGS += -std=c++11
 AR      = ar
 ARFLAGS += rvs
 
-ifeq ($(OS),Windows_NT)
-	MKDIR  = mkdir
-	RM     = del
-	RMDIR  = rmdir /s /Q
-	COPY   = copy
-	MOVE   = move
-	RENAME = ren
-	NULL_DEVICE = nul
-else
-	MKDIR  = mkdir -p
-	RM     = rm -f
-	RMDIR  = rm -r
-	COPY   = cp
-	MOVE   = mv -f
-	RENAME = mv -f
-	NULL_DEVICE = /dev/null
-endif
+MKDIR  = mkdir -p
+RM     = rm -f
+RMDIR  = rm -rf
+COPY   = cp
+MOVE   = mv -f
+RENAME = mv -f
+NULL_DEVICE = /dev/null
